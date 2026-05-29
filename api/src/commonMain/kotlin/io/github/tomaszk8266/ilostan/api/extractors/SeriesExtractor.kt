ace71e94287fa7aa118b7c44fc47c6bdd5b8b495
Tomasz K. <tomaszk8266@proton.me>
2026-05-29 23:42:35 +0200

    API: Fix manufacturing data regex in SeriesExtractor

--- api/src/commonMain/kotlin/io/github/tomaszk8266/ilostan/api/extractors/SeriesExtractor.kt
+++ api/src/commonMain/kotlin/io/github/tomaszk8266/ilostan/api/extractors/SeriesExtractor.kt
@@ -21,7 +21,7 @@ suspend fun getAndExtractSeries(id: Int) = Series(
             val manufacturingData = it.selectFirst("td:first-child")
                 ?.textNodes()[0]!!.text()
                 .trimQuotes().trim()
-            val manufacturingDataRegex = Regex("""([a-zA-Z0-9-_]+) (?:\((\d{4})\))?""")
+            val manufacturingDataRegex = Regex("""(?:([a-zA-Z0-9-_]+)\s)?(?:\((\d{4})\))?""")
             val manufacturingDataExtracted = manufacturingDataRegex.find(manufacturingData)?.groupValues
                 ?: return@mapNotNull null
 
@@ -49,7 +49,7 @@ suspend fun getAndExtractSeries(id: Int) = Series(
     photos = Ksoup.parse("https://ilostan.forumkolejowe.pl/index.php?nav=nowe_foto&seria=$id")
         .body().select("div.main > div.text:nth-child(8) tbody > tr:not(.naglowektab)").map { row ->
             Photo(
-                id = Regex("""^foto/(\d+).""")
+                id = Regex("""^foto/(\d+)\.""")
                     .find(row.select("td.foto img").attr("src"))!!
                     .groupValues[1].toInt(),
                 date = row.selectFirst("td.tab_koment b")?.text()?.parseDate()!!,
